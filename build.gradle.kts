d31c33ac7a86df703db2298d5a57cd341fd67f37
exoego <ytatsuno.jp@gmail.com>
2024-10-30 17:13:33 +0900

    Setup JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,17 @@ repositories {
 group = "io.github.grimmjo"
 version = "0.4"
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 dependencies {
     implementation(gradleApi())
     implementation("com.sebastian-daschner:jaxrs-analyzer:0.17")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
 }
 
 gradlePlugin {
---
+++ src/test/kotlin/functional/JaxRsAnalyzerPluginFunctionalTest.kt
@@ -0,0 +1,11 @@
+package functional
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class JaxRsAnalyzerPluginFunctionalTest {
+    @Test
+    fun test() {
+        assertEquals(true, true)
+    }
+}
